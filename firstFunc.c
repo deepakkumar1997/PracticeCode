18ccd94a2b95139165da423c9de1d42e4cd1b2eb
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2021-06-09 10:04:18 +0530

    Added initial function programs

---
+++ firstFunc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void printFunc()
+{
+    printf("This is my first function of this type.\nMeans Func Definition 1st then func calling.\n");
+}
+int main()
+{
+    printFunc();
+    return 0;
+}
